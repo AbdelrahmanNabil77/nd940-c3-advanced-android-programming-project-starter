93141a941a08b130dcdffddd8aa8c19bbdeec739
AbdElRahman Nabil <abdelrahmann89@gmail.com>
2022-11-16 22:46:01 +0200

    added the radio buttons logic

--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -8,10 +8,12 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import com.udacity.utils.DownloadUtils
 import com.udacity.utils.createChannel
 import com.udacity.utils.sendNotification
@@ -20,12 +22,10 @@ import kotlinx.android.synthetic.main.content_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
-    private var downloadID: Long = 0
-
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
+    private var chosenBtn: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,9 +44,13 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         custom_button.setOnClickListener {
-            custom_button.changeButtonState(ButtonState.Loading)
-            Toast.makeText(this@MainActivity, "Download started", Toast.LENGTH_SHORT).show()
-            DownloadUtils.download(this, URL)
+            if (isBtnChosen()) {
+                custom_button.changeButtonState(ButtonState.Loading)
+                DownloadUtils.download(this, getChosenLink())
+            } else {
+                Toast.makeText(this@MainActivity, "Choose an option first", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
 
@@ -54,23 +58,21 @@ class MainActivity : AppCompatActivity() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
             if (DownloadUtils.isDownloadSuccess(this@MainActivity, id!!)) {
-                Toast.makeText(this@MainActivity, "DONE SUCCESS", Toast.LENGTH_LONG).show()
                 notificationManager.sendNotification(
                     "done",
                     "downloaded successfully",
                     this@MainActivity,
                     id,
-                    "PICTURE"
+                    "${getChosenTitle()}"
                 )
                 custom_button.changeButtonState(ButtonState.Completed)
             } else {
-                Toast.makeText(this@MainActivity, "DONE FAIL", Toast.LENGTH_LONG).show()
                 notificationManager.sendNotification(
-                    "done",
+                    "fail",
                     "failed download",
                     this@MainActivity,
                     id,
-                    "PICTURE"
+                    "${getChosenTitle()}"
                 )
                 custom_button.changeButtonState(ButtonState.Completed)
             }
@@ -78,8 +80,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val URL =
+        private const val UDACITY_URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/refs/heads/master.zip"
+        private const val RETROFIT_URL =
+            "https://github.com/square/retrofit/archive/refs/heads/master.zip"
+        private const val GLIDE_URL =
+            "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
+
     }
 
+    fun isBtnChosen(): Boolean {
+        return rg_download_choices.checkedRadioButtonId != -1
+    }
+
+    fun getChosenLink(): String {
+        var link = ""
+        when (rg_download_choices.checkedRadioButtonId) {
+            rb_glide.id -> link = GLIDE_URL
+            rb_retrofit.id -> link = RETROFIT_URL
+            rb_load_app.id -> link = UDACITY_URL
+        }
+        return link
+    }
+
+    fun getChosenTitle(): String {
+        val btnTxt =
+            findViewById<RadioButton>(rg_download_choices.checkedRadioButtonId).text.toString()
+        return btnTxt
+    }
 }
